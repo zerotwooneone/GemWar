d0b60b4c3e83172676ca3a63f8040bd0ce89541f
David Squire <squiredd@gmail.com>
2018-03-20 21:16:46 -0700

    fixed side-nav new sheet test errors

--- charsheet/src/app/side-nav/new-sheet/new-sheet.component.spec.ts
+++ charsheet/src/app/side-nav/new-sheet/new-sheet.component.spec.ts
@@ -5,6 +5,9 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { SaveResult } from '../../form/save-result';
 import { FormSaveService } from '../../form/form-save.service';
 import { Router } from '@angular/router';
+import { MatIconModule } from '@angular/material';
+import { MockFormSaveService } from '../../../testing/mock-services';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('NewSheetComponent', () => {
   let component: NewSheetComponent;
@@ -15,7 +18,9 @@ describe('NewSheetComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [NewSheetComponent]
+      declarations: [NewSheetComponent],
+      imports: [MatIconModule, RouterTestingModule],
+      providers: [{ provide: FormSaveService, useClass: MockFormSaveService }]
     })
       .compileComponents();
   }));
